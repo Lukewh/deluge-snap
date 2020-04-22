547bf21bca685b5c8f432453953e999322466112
Lukewh <luke@lukewh.com>
2020-04-22 13:52:49 +0100

    Update launcher to create files and set config dir

--- launcher.sh
+++ launcher.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
-if [ -d "$SNAP_USER_DATA/.config/deluge" ]; then
-    echo "$SNAP_USER_DATA exists"
+COMMON_DOWNLOADS_DIR="$SNAP_USER_COMMON/Downloads"
+COMMON_CONFIGS_DIR="$SNAP_USER_COMMON/.config/deluge"
+
+# Make a common downloads folder if it doesn't exist
+if [ -d "$COMMON_DOWNLOADS_DIR" ]; then
+    echo "$COMMON_DOWNLOADS_DIR exists already! Yay!"
 else
-    mkdir -p "$SNAP_USER_DATA/.config/deluge/"
+    echo "Make downloads directory: $COMMON_DOWNLOADS_DIR"
+    mkdir "$COMMON_DOWNLOADS_DIR"
 fi
 
-exec "$@"
+if [ -d "$COMMON_CONFIGS_DIR" ]; then
+    echo "$COMMON_CONFIGS_DIR exists already! Yay!"
+else
+    echo "Make config directory in common directory: $COMMON_CONFIGS_DIR"
+    mkdir -p "$COMMON_CONFIGS_DIR"
+fi
+
+echo "$@" -c "$COMMON_CONFIGS_DIR/"
+
+exec "$@" -c "$COMMON_CONFIGS_DIR/"
